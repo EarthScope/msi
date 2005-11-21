cc3c9997e9d70a7dfad6b7a9546e8104238ef8e5
Chad Trabant <chad@iris.washington.edu>
2005-11-21 19:41:21 +0000

    update libmseed to 1.4.4

--- src/msi.c
+++ src/msi.c
@@ -26,7 +26,7 @@ static int lisnumber (char *number);
 static void addfile (char *filename);
 static void usage (void);
 
-#define VERSION "1.4"
+#define VERSION "1.5"
 #define PACKAGE "msi"
 
 static flag    verbose      = 0;
