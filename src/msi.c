7bd85e01f542a47111f86ce4c0f43754579c8e91
Chad Trabant <chad@iris.washington.edu>
2009-07-13 23:23:55 +0000

    *** empty log message ***

--- src/msi.c
+++ src/msi.c
@@ -31,7 +31,7 @@ static int addfile (char *filename);
 static int addlistfile (char *filename);
 static void usage (void);
 
-#define VERSION "3.1dev"
+#define VERSION "3.1rc"
 #define PACKAGE "msi"
 
 static flag    verbose      = 0;
