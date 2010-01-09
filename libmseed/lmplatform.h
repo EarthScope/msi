201a5678c642f6aa4c4acc8004c54ad69733a9bd
Chad Trabant <chad@iris.washington.edu>
2010-01-09 04:50:05 +0000

    *** empty log message ***

--- libmseed/lmplatform.h
+++ libmseed/lmplatform.h
@@ -18,7 +18,7 @@
  *
  * Written by Chad Trabant, IRIS Data Management Center
  *
- * modified: 2009.177
+ * modified: 2010.006
  ***************************************************************************/
 
 #ifndef LMPLATFORM_H
@@ -27,7 +27,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-  
+
+  /* On some platforms (e.g. ARM) structures are aligned on word boundaries
+     by adding padding between the elements.  This library uses structs that
+     map to SEED header/blockette structures that are required to have a
+     layout exactly as specified, i.e. no padding.
+
+     If "ATTRIBUTE_PACKED" is defined at compile time (e.g. -DATTRIBUTE_PACKED)
+     the preprocessor will use the define below to add the "packed" attribute 
+     to effected structs.  This attribute is supported by GCC and increasingly
+     more compilers.
+  */
+#if defined(ATTRIBUTE_PACKED)
+  #define LMP_PACKED __attribute__((packed))
+#else
+  #define LMP_PACKED
+#endif
+
   /* Make some guesses about the system libraries based
    * on the architecture.  Currently the assumptions are:
    * Linux => glibc2 libraries (LMP_GLIBC2)
--- src/msi.c
+++ src/msi.c
@@ -31,7 +31,7 @@ static int addfile (char *filename);
 static int addlistfile (char *filename);
 static void usage (void);
 
-#define VERSION "3.1"
+#define VERSION "3.2dev"
 #define PACKAGE "msi"
 
 static flag    verbose      = 0;
